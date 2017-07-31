2381ff2ceafffa8acc1c8daaa3ed071b76c97eb4
Kristin Glaze <ikglaze33@gmail.com>
2017-07-31 00:26:10 -0400

    Includes ratings with gif images. Button that is active shows css that highlights its active-ness. Able to add buttons with search terms that contain spaces.

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
-	var initialButtonValues = ["dove", "turtle", "ostrich"];
+	var currentButtonValues = ["dove", "turtle", "ostrich"];
 	const API_KEY = "69777a707ff64e1c8accdcc450d9c54c";
 	const LIMIT = 5;
-	for(var i = 0; i < initialButtonValues.length; i++) {
-		addGifButton(initialButtonValues[i]);
+	for(var i = 0; i < currentButtonValues.length; i++) {
+		addGifButton(currentButtonValues[i]);
 	}
 	$(".add-gif-button").click(function(event) {
 		event.preventDefault();
 		var buttonAdditionValue = $(".add-gif-input").val();
 		addGifButton(buttonAdditionValue);
 	});
+	function prepStringForUrl(str) {
+		str = str.replace(/\s+/g, '+').toLowerCase();
+		return str;
+	}
 	function addGifButton(textInButton) {
-		var $button = $("<button>").addClass("search-gifs-button").attr("data-search-word", textInButton).text(textInButton);
-		$(".gif-buttons-added").append($button);
-		$button.click(function(event) {
-			var buttonSearchValue = $(this).attr("data-search-word");
-			searchGiphyApi(buttonSearchValue);
-		});
-		$(".add-gif-input").val("");
+		if(textInButton.length > 0) {
+			var $button = $("<button>").addClass("search-gifs-button btn").attr("data-search-word", prepStringForUrl(textInButton)).text(textInButton);
+			$(".gif-buttons-added").append($button);
+			$button.click(function(event) {
+				var buttonSearchValue = $(this).attr("data-search-word");
+				$(".search-gifs-button").removeClass("active");
+				$(this).addClass("active");
+				searchGiphyApi(buttonSearchValue);
+			});
+			$(".add-gif-input").val("");
+		}
 	}
 	function appendGiphyResults(results) {
 		$(".giphy-results").append(results);
@@ -26,7 +34,6 @@ $(document).ready(function() {
 		$(".giphy-results .gif").hide();
 	}
 	function searchGiphyApi(gihpySearchTerm) {
-		debugger;
 		clearGiphyResults();
 		if($(".gif-" + gihpySearchTerm).length > 0) {
 			$(".gif-" + gihpySearchTerm).show();
@@ -36,18 +43,19 @@ $(document).ready(function() {
 			xhr.done(function(response) {
 				var responseData = response.data;
 				// console.log("success got data", responseData);
-				//clearGiphyResults();
 				for (var i = 0; i < Math.min(responseData.length, LIMIT); i++) {
 					// console.log(responseData[i].data);
 					var gifUrlStill = responseData[i].images.fixed_height_still.url;
 					var gifUrlAnimated = responseData[i].images.fixed_height.url;
-					$gifImg = $("<img>").attr("src", gifUrlStill).addClass("gif gif-" + gihpySearchTerm);
+					$gifImg = $("<img>").attr("src", gifUrlStill).addClass("gif gif-" + gihpySearchTerm.replace('+', '-'));
 					$gifImg.attr("data-still", gifUrlStill);
 					$gifImg.attr("data-animated", gifUrlAnimated);
 					$gifImg.attr("data-state", "still");
-					appendGiphyResults($gifImg);
+					var $gifImgDiv = $("<div>").addClass("gif gif-" + gihpySearchTerm.replace('+', '-')).html($gifImg);
+					$gifImgDiv.prepend($("<p>").text("Rating: " + responseData[i].rating.toUpperCase()));
+					appendGiphyResults($gifImgDiv);
 				}
-				$(".gif-" + gihpySearchTerm).click(function(event) {
+				$(".gif-" + gihpySearchTerm.replace('+', '-')).click(function(event) {
 					if($(this).attr("data-state") === "still") {
 						$(this).attr("src", $(this).attr("data-animated"));
 						$(this).attr("data-state", "animated");
@@ -60,4 +68,3 @@ $(document).ready(function() {
 		}
 	}
 });
-0
